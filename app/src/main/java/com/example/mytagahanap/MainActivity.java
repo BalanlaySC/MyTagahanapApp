c936d8e0959840556bebf8ba5e6077f4f9415b82
BalanlaySC <balanlaysc@gmail.com>
2021-11-24 00:56:17 +0800

    Search feature is complete but lacks direction

--- app/src/main/java/com/example/mytagahanap/MainActivity.java
+++ app/src/main/java/com/example/mytagahanap/MainActivity.java
@@ -6,12 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
+import androidx.core.view.MenuItemCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -19,10 +26,17 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MainActivity";
+    private static final String[] COUNTRIES = new String[] { "Belgium",
+            "France", "France_", "Italy", "Germany", "Spain" };
+
     private DatabaseAccess databaseAccess;
+    private ArrayAdapter<String> arrayAdapter;
+
+    private AutoCompleteTextView autoCompleteTextView;
     private Toolbar toolbar;
     private DrawerLayout drawer;
     private NavigationView navigationView;
+    private ArrayList<String> locations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +44,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 
         initViews();
-
-        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(getApplicationContext());
-        databaseAccess.openDatabase();
-
-        ArrayList<String> locations = new ArrayList<String>();
-        locations = databaseAccess.getAllLocations();
-        // TODO now proceed to autocomplete using locations ArrayList
-        databaseAccess.closeDatabase();
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
@@ -48,6 +54,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private void initViews() {
         toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.other_menu);
+
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.main_drawer_layout);
@@ -59,30 +67,61 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        databaseAccess = DatabaseAccess.getInstance(getApplicationContext());
+        databaseAccess.openDatabase();
+
+        locations = new ArrayList<String>();
+        locations = databaseAccess.getAllLocations();
+        // TODO now proceed to autocomplete using locations ArrayList
+        databaseAccess.closeDatabase();
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.other_menu, menu);
-//
-//        MenuItem menuItem = menu.findItem(R.id.actionSearch);
-//        SearchView searchView = (SearchView) menuItem.getActionView();
-//        searchView.setQueryHint("Type here to search");
-//
-//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//            @Override
-//            public boolean onQueryTextSubmit(String query) {
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean onQueryTextChange(String newText) {
-//                return false;
-//            }
-//        });
-//
-//        return super.onCreateOptionsMenu(menu);
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the search menu action bar.
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.other_menu, menu);
+
+        // Get the search menu.
+        MenuItem searchMenu = menu.findItem(R.id.actionSearch);
+
+        // Get SearchView object.
+        SearchView searchView = (SearchView) searchMenu.getActionView();
+
+        // Get SearchView autocomplete object.
+        final SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete) searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        searchAutoComplete.setDropDownBackgroundResource(android.R.color.holo_blue_light);
+
+        // Create a new ArrayAdapter and add data (locations) to search auto complete object.
+        ArrayAdapter<String> newsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, locations);
+        searchAutoComplete.setAdapter(newsAdapter);
+
+        // Listen to search view item on click event.
+        searchAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int itemIndex, long id) {
+                String queryString = (String) adapterView.getItemAtPosition(itemIndex);
+                searchAutoComplete.setText(queryString);
+                // TODO proceed to then get then mark the map
+            }
+        });
+
+        // Below event is triggered when submit search query.
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // TODO Refer to the setOnItemClickListener
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return true;
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -94,10 +133,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_subjects:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new ScheduleFragment()).commit();
-                break;
-            case R.id.nav_search:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new SearchFragment()).commit();
                 break;
             case R.id.nav_bldginfo:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
