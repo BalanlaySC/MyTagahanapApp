7b7adbab31ecf575411aa11695639fc53ca0a73e
Jdgzmn <deguzmanecyoj@gmail.com>
2021-12-30 10:33:38 +0800

    Setup interface, Added SubjectModel Added an ArrayList with dummy data

--- app/src/main/java/com/example/mytagahanap/ScheduleFragment.java
+++ app/src/main/java/com/example/mytagahanap/ScheduleFragment.java
@@ -1,5 +1,6 @@
 package com.example.mytagahanap;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,10 +9,33 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class ScheduleFragment extends Fragment {
+    private Context scheduleFragmentContext;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        scheduleFragmentContext = getContext().getApplicationContext();
+        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        ArrayList<SubjectModel> classSchedule = new ArrayList<>();
+        classSchedule.add(new SubjectModel("0011660",
+                "Integrative Programming and Technologies 2", "IPT102",
+                "L1", "1PM -> 2PM", "MonWedFri"));
+        classSchedule.add(new SubjectModel("0011660",
+                "Integrative Programming and Technologies 2", "IPT102",
+                "CSFLD9", "8AM -> 9AM", "MonWed"));
+        classSchedule.add(new SubjectModel("0011730",
+                "Information Assurance and Security 2", "IAS102",
+                "L3", "1030AM -> 12PM", "MonWed"));
+        classSchedule.add(new SubjectModel("0011730",
+                "Information Assurance and Security 2", "IAS102",
+                "CCS OFC", "5PM -> 7PM", "Mon"));
+        classSchedule.add(new SubjectModel("0011850",
+                "The Entrepreneurial Mind", "GE ELEC 3",
+                "CS204", "830AM -> 10AM", "TueThu"));
+
+        return view;
     }
 }
